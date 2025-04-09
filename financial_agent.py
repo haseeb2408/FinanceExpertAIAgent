42b070c3c41471e0810582265457fcee69df8837
Haseeb Ahmed <ahmed0499280@gmail.com>
2025-04-09 11:40:05 +0500

    OpenAI Issue resolved

--- financial_agent.py
+++ financial_agent.py
@@ -2,15 +2,16 @@ from phi.agent import Agent
 from phi.model.groq import Groq
 from phi.tools.yfinance import YFinanceTools
 from phi.tools.duckduckgo import DuckDuckGo
+from dotenv import load_dotenv
 
-
+load_dotenv()
 ## websearch Agent
 websearch_agent = Agent(
     name = "Web Search Agent",
     role = "Search the web for the information" , 
-    model = Groq(id = 'llama3-groq-70b-8192-tool-use-preview'),
+    model = Groq(id = 'llama-3.3-70b-versatile'),
     tools = [DuckDuckGo()],
-    instructions='Always include the resource',
+    instructions=['Always include the resource'],
     show_tool_calls=True,
     markdown=True
 )
@@ -19,7 +20,7 @@ websearch_agent = Agent(
 ## Financial Agent
 financial_agent = Agent(
     name = "Financial Agent" , 
-    model = Groq(id = 'llama3-groq-70b-8192-tool-use-preview'),
+    model = Groq(id = 'llama-3.3-70b-versatile'),
     tools =[
         YFinanceTools(stock_price=True, analyst_recommendations=True, stock_fundamentals=True , company_news = True)
         ],
@@ -30,6 +31,7 @@ financial_agent = Agent(
 
 
 multi_agent = Agent(
+    model = Groq(id='llama-3.3-70b-versatile'),
     team = [websearch_agent , financial_agent] , 
     instructions = ['Always include the resource' , 'Use tables to display the Data'] , 
     show_tool_calls=True,
@@ -37,4 +39,4 @@ multi_agent = Agent(
 )
 
 
-multi_agent.print_response("Summarize analyst recomendations and share the lateast new for NVDA" , stream = True)
+multi_agent.print_response("Summarize analyst recomendations and share the lateast news about NVDA" , stream = True)
--- playground.py
+++ playground.py
@@ -3,6 +3,9 @@ from phi.model.groq import Groq
 from phi.tools.yfinance import YFinanceTools
 from phi.tools.duckduckgo import DuckDuckGo
 import phi.api
+from dotenv import load_dotenv
+
+load_dotenv()
 
 
 import os
@@ -15,9 +18,9 @@ phi.api = os.getenv("PHI_API_KEY")
 websearch_agent = Agent(
     name = "Web Search Agent",
     role = "Search the web for the information" , 
-    model = Groq(id = 'llama3-groq-70b-8192-tool-use-preview'),
+    model = Groq(id = 'llama-3.3-70b-versatile'),
     tools = [DuckDuckGo()],
-    instructions='Always include the resource',
+    instructions=['Always include the resource'],
     show_tool_calls=True,
     markdown=True
 )
@@ -26,7 +29,7 @@ websearch_agent = Agent(
 ## Financial Agent
 financial_agent = Agent(
     name = "Financial Agent" , 
-    model = Groq(id = 'llama3-groq-70b-8192-tool-use-preview'),
+    model = Groq(id = 'llama-3.3-70b-versatile'),
     tools =[
         YFinanceTools(stock_price=True, analyst_recommendations=True, stock_fundamentals=True , company_news = True)
         ],
